2546b593ad8d6c4e40d53f6d2dc364cc34310fc3
Komár Csongor <csongi8916@gmail.com>
2024-03-27 05:54:01 +0100

    fix anagram minor issues and word chain implementation is in progress

--- client/src/app/control-container/control-container.component.ts
+++ client/src/app/control-container/control-container.component.ts
@@ -1,6 +1,6 @@
-import { WordGameService } from './../../core/word-game.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { WordGameService } from './../../core/word-game.service';
 
 @Component({
   selector: 'lp-control-container',
@@ -9,22 +9,32 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ControlContainerComponent implements OnInit {
 
+  @Input() title: string;
+  @Input() isNeedDest: boolean;
+
   wordGameForm: FormGroup;
+  actualSrcWord: string = '';
+  actualDestWord: string = '';
   anagrams: string[] = [];
 
   constructor(private wordGameService: WordGameService) { }
   
   ngOnInit(): void {
     this.wordGameForm = new FormGroup({
-      word: new FormControl(null, [Validators.min(5), Validators.max(5)]),
+      sourceWord: new FormControl(null, [Validators.min(5), Validators.max(5)]),
+      destWord: new FormControl(null, [Validators.min(5), Validators.max(5)]),
     });
   }
 
   onSubmit(): void {
-    const word = this.wordGameForm.get('word')?.value;
-    this.wordGameService.getAnagrams(word).subscribe((res: any) => {
+    this.actualSrcWord = this.wordGameForm.get('sourceWord')?.value;
+    this.wordGameService.getAnagrams(this.actualSrcWord).subscribe((res: any) => {
       this.anagrams = res.body.anagrams;
-      this.wordGameService.moveResults({ actualWord: word, words: this.anagrams });
+      this.wordGameService.moveResults({ actualWord: this.actualSrcWord, words: this.anagrams });
     });
+    /*if (this.isNeedDest) {
+      this.actualDestWord = this.wordGameForm.get('destWord')?.value;
+    }*/
   }
+
 }
--- client/src/app/result-container/result-container.component.ts
+++ client/src/app/result-container/result-container.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { WordGameService } from 'src/core/word-game.service';
 import { Result } from 'src/types.model';
 
@@ -7,9 +8,11 @@ import { Result } from 'src/types.model';
   templateUrl: './result-container.component.html',
   styleUrls: ['./result-container.component.scss']
 })
-export class ResultContainerComponent implements OnInit {
+export class ResultContainerComponent implements OnInit, OnDestroy {
 
   @Input() actualWord: string;
+
+  private resultSub: Subscription;
   results: string[] = [];
   isEmpty: boolean = false;
   isAlreadyUsed: boolean = false;
@@ -17,7 +20,7 @@ export class ResultContainerComponent implements OnInit {
   constructor(private wordGameService: WordGameService) { }
 
   ngOnInit(): void {
-    this.wordGameService.moveResultEmitter$.subscribe((results: Result) => {
+    this.resultSub = this.wordGameService.moveResultEmitter$.subscribe((results: Result) => {
       this.isEmpty = results.words.length === 0 ? true : false;
       this.isAlreadyUsed = true;
       if (this.actualWord !== results.actualWord) {
@@ -25,5 +28,9 @@ export class ResultContainerComponent implements OnInit {
         this.results = results.words;
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.resultSub.unsubscribe();
   }
 }
